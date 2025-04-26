3a3fe875958397f9b24dff90c588597dfcb049d5
giotechadmin <giotech_admin@giortech.com>
2025-04-26 13:28:46 -0500

    fixed

--- terraform/modules/workload_identity/main.tf
+++ terraform/modules/workload_identity/main.tf
@@ -1,7 +1,7 @@
 # Create Workload Identity Pool for GitHub Actions
 resource "google_iam_workload_identity_pool" "github_pool" {
   count = var.create_identity_pool ? 1 : 0
-  
+
   workload_identity_pool_id = "github-pool"
   display_name              = "GitHub Actions Pool"
   description               = "Identity pool for GitHub Actions"
@@ -11,19 +11,19 @@ resource "google_iam_workload_identity_pool" "github_pool" {
 # Create Workload Identity Provider for GitHub
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
   count = var.create_identity_pool ? 1 : 0
-  
+
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool[0].workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
   display_name                       = "GitHub Provider"
   project                            = var.project_id
-  
+
   attribute_mapping = {
-    "google.subject"         = "assertion.sub"
-    "attribute.actor"        = "assertion.actor"
-    "attribute.repository"   = "assertion.repository"
+    "google.subject"             = "assertion.sub"
+    "attribute.actor"            = "assertion.actor"
+    "attribute.repository"       = "assertion.repository"
     "attribute.repository_owner" = "assertion.repository_owner"
   }
-  
+
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
@@ -34,7 +34,7 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
 # Create Service Account for GitHub Actions
 resource "google_service_account" "github_actions_sa" {
   count = var.create_service_account ? 1 : 0
-  
+
   account_id   = "github-actions-sa"
   display_name = "GitHub Actions Service Account"
   project      = var.project_id
@@ -44,10 +44,10 @@ resource "google_service_account" "github_actions_sa" {
 # Allow GitHub Actions to use this Service Account
 resource "google_service_account_iam_binding" "workload_identity_binding" {
   count = var.create_service_account && var.create_identity_pool ? 1 : 0
-  
+
   service_account_id = google_service_account.github_actions_sa[0].name
   role               = "roles/iam.workloadIdentityUser"
-  
+
   members = [
     "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/github-pool/attribute.repository/${var.github_org}/${var.github_repo}"
   ]
--- terraform/organization/giortech/dev/main.tf
+++ terraform/organization/giortech/dev/main.tf
@@ -15,19 +15,19 @@ terraform {
 # Include the parent module
 module "giortech" {
   source = "../"
-  
+
   # Pass the variables specific to dev environment
   project_id         = "giortech-dev-project"
   environment        = "dev"
   region             = "us-central1"
   billing_account_id = "0141E4-398D5E-91A063"
   budget_amount      = 50
-  
+
   # Additional variables to handle existing resources
-  create_identity_pool = false
+  create_identity_pool   = false
   create_service_account = false
-  deploy_cloud_run = false
-  create_budget = false
+  deploy_cloud_run       = false
+  create_budget          = false
 }
 
 # Output the Workload Identity Provider and Service Account
