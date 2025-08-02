d42666ed525c8a343a4b438dadbc8cf36e778727
giotechadmin <giotech_admin@giortech.com>
2025-08-02 09:36:07 +0100

    feat: Add academyaxis237 support to app-deploy workflow

---
+++ terraform/organization/academyaxis237/prod/variables.tf
@@ -0,0 +1,53 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "academyaxis-237-prod-project"
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "environment" {
+  description = "Environment (dev, uat, prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "billing_account_id" {
+  description = "Billing account ID"
+  type        = string
+  default     = "0156AD-1517D4-139949"  # AcademyAxis-237 billing account
+}
+
+variable "budget_amount" {
+  description = "Monthly budget amount in USD"
+  type        = number
+  default     = 100
+}
+
+variable "alert_email_address" {
+  description = "Email address for alerts"
+  type        = string
+  default     = "admin@giortech.com"
+}
+
+variable "create_identity_pool" {
+  description = "Whether to create the workload identity pool"
+  type        = bool
+  default     = false
+}
+
+variable "create_service_account" {
+  description = "Whether to create the service account"
+  type        = bool
+  default     = false
+}
+
+variable "create_budget" {
+  description = "Whether to create budget"
+  type        = bool
+  default     = true
+}
